d029b139d2d7e766c840d65e8b349b4469609462
Paul Kail <pkail@runbox.com>
2022-07-11 18:56:19 -0700

    Added files inside sankey and barcharts directories

---
+++ components/barcharts/organSlice.js
@@ -0,0 +1,19 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+
+const organSlice = createSlice({
+	name: 'dataSelection',
+	initialState: ["Spleen", "Liver", "Heart"],
+	reducers: {
+		all: (state) => ["Spleen", "Liver", "Heart"],
+		spleen: (state) => ["Spleen"],
+		liver: (state) => ["Liver"],
+		heart: (state) => ["Heart"],
+	}
+})
+export const {spleen, liver, heart, all} = organSlice.actions;
+
+export default organSlice.reducer;
+export const selectOrgan = state => state;
+
+
