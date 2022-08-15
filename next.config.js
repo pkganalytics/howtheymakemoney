2db9c7ecb40685b6022c6029132828ff9d8a0867
Paul Kail <pkail@runbox.com>
2022-08-14 21:02:47 -0700

    Upgraded webpack-dev-server; removed node-sass

--- next.config.js
+++ next.config.js
@@ -8,36 +8,3 @@ module.exports = withPlugins(
 	[withurlLoader,
 		{ test: /\.(woff2|woff|ttf|eot|svg|png|jpe?g|gif|svg|ico)(\?v=[a-z0-9]\.[a-z0-9]\.[a-z0-9])?$/, use: 'url-loader?limit=100000' }
 	])
-
-// module.exports = {
-//   module: {
-//     rules: [
-//       {
-//         test: /\.(png|jpg|gif)$/i,
-//         use: [
-//           {
-//             loader: 'url-loader',
-//             options: {
-//               limit: 8192,
-//             },
-//           },
-//         ],
-//       },
-//     ],
-//   },
-// };
-// module.exports = withCSS(withSass({
-//   webpack (config, options) {
-//     config.module.rules.push({
-//       test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
-//       use: {
-//         loader: 'url-loader',
-//         options: {
-//           limit: 100000
-//         }
-//       }
-//     })
-//     return config
-//   }
-// }))
-
