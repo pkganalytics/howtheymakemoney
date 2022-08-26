62fe50e6a95e659f6f027ed1ed71b15c186f7acc
Paul Kail <pkail@runbox.com>
2022-08-26 16:48:17 -0700

    Added colourSlice

---
+++ components/sankey/colourSlice.js
@@ -0,0 +1,17 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { colours } from "./sankeyData";
+
+const colourSlice = createSlice({
+	name: 'colours',
+	initialState: colours[1],
+	reducers: {
+		setColour0: (state) => colours[0],
+		setColour1: (state) => colours[1],
+		setColour2: (state) => colours[2],
+		setColour3: (state) => colours[3]
+	}
+})
+
+export const {setColour0, setColour1, setColour2, setColour3} = colourSlice.actions;
+
+export default colourSlice.reducer;
