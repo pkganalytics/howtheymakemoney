76cb1e4928b7a45b248662dfff283ab1e12ecb2f
Paul Kail <pkail@runbox.com>
2022-09-17 19:19:39 -0700

    Removed image and wrong file which was flagged

--- components/subset/subsetSidePanel.js
+++ components/subset/subsetSidePanel.js
@@ -5,13 +5,6 @@ const SidePanel = () => {
 	return (
 		<div className="sidePanel">
 			<div>
-				<img
-					src="/twowomenwhiteboard.jpg"
-					srcSet="/twowomenwhiteboard.jpg 1000w, /twowomenwhitboard_m.jpg 400w, /twowomenwhiteboard_s 200w"
-					sizes="(max-width: 600px) 100vw, (min-width: 601px) 35vw"
-					type="image/jpg"
-					className="sidePanelImage"
-				/>
 			</div>
 		</div>
 	);
