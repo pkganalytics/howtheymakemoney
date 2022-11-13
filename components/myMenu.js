ad45e7f9271f9c7c658edaa9acd50e50ea4dd317
Paul Kail <pkail@runbox.com>
2022-11-13 12:53:40 -0800

    Added other pages

--- components/myMenu.js
+++ components/myMenu.js
@@ -13,12 +13,34 @@ import Typography from '@mui/material/Typography';
 const indexToTabName= {
 	home: 0,
 	wrk: 1,
+	ip: 2,
+	cclb: 3,
+	pkg: 4,
+	amcr: 5,
+	gpk: 6,
+	son: 7,
+	see: 8,
+	vrtv: 9,
+	ball: 10,
+	ambp: 11,
+	gef: 12
 }
 
 
 const tabNametoIndex = {
 	0: "/",
 	1: "/wrk",
+	2: "/ip",
+	3: "/cclb",
+	4: "/pkg",
+	5: "/amcr",
+	6: "/gpk",
+	7: "/son",
+	8: "/see",
+	9: "/vrtv",
+	10: "/ball",
+	11: "/ambp",
+	12: "/gef"
 }
 
 const MyMenu = () => {
@@ -41,8 +63,19 @@ const MyMenu = () => {
 		value={indexToTabName[router.pathname.substring(1)] || 0}
 		onChange={handleChange}
 		indicatorColor='secondary'>
-            <Tab label='Home' sx={{width: 1/7}} />
-			<Tab label='WRK' sx={{width: 1/7}} />
+            <Tab label='Home' sx={{width: 1/14}} />
+			<Tab label='WRK' sx={{width: 1/14}} />
+			<Tab label='IP' sx={{width: 1/14}} />
+			<Tab label='CCL.B' sx={{width: 1/14}} />
+			<Tab label='PKG' sx={{width: 1/14}} />
+			<Tab label='AMCR' sx={{width: 1/14}} />
+			<Tab label='GPK' sx={{width: 1/14}} />
+			<Tab label='SON' sx={{width: 1/14}} />
+			<Tab label='SEE' sx={{width: 1/14}} />
+			<Tab label='VRTV' sx={{width: 1/14}} />
+			<Tab label='BALL' sx={{width: 1/14}} />
+			<Tab label='AMBP' sx={{width: 1/14}} />
+			<Tab label='GEF' sx={{width: 1/14}} />
         </Tabs>
 	</AppBar>
     )
